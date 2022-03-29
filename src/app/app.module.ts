49106321f0444be6e9d787f9a987a7971d62495e
ianacodev <ianaco.dev@gmail.com>
2022-03-29 10:56:50 -0400

    add angular flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 // ngrx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -29,6 +30,7 @@ import { environment } from '../environments/environment';
       logOnly: environment.production,
       autoPause: true,
     }),
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
