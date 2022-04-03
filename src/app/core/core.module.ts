0269837202fe67767c2b589cddeefe6c456b6950
ianacodev <ianaco.dev@gmail.com>
2022-04-03 15:40:42 -0400

    add page not found

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 // components
 import * as fromComponents from './components';
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [...fromComponents.components],
 })
 export class CoreModule {}
